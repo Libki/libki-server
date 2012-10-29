2204c6328e28e6695f38e4c2ed985f80a81950e6
Kyle M Hall <kyle@kylehall.info>
2012-10-29 09:13:42 -0400

    Update license in files. Delete jqGrid unit test.

--- script/migration/create_passwords.pl
+++ script/migration/create_passwords.pl
@@ -5,10 +5,14 @@ use warnings;
 
 use Config::JFDI;
 
+use FindBin;
+use lib "$FindBin::Bin/../../lib";
+
 use Libki::Model::DB;
 use Libki::Schema::DB;
 
-my $config = Config::JFDI->new( file => '/home/libki/Libki/libki_local.conf' );
+my $config =
+  Config::JFDI->new( file => "$FindBin::Bin/../../libki_local.conf", no_06_warning => 1 );
 my $config_hash  = $config->get;
 my $connect_info = $config_hash->{'Model::DB'}->{'connect_info'};
 
@@ -18,12 +22,34 @@ my $schema = Libki::Schema::DB->connect($connect_info)
 my @users = $schema->resultset('User')->all;
 
 foreach my $user (@users) {
-    print "Working on " . $user->username() . "\n";
-    print "password hash was: " . $user->password() . "\n";
+    print "Working on " . $user->username() . "...\n";
 
-    $user->password('letmein');
-    $user->update;
-
-    print "password hash is now: " . $user->password() . "\n";
-
+    $user->password( $user->username() );
+    $user->update();
 }
+
+print "Done. Passwords are set to usernames.\n\n";
+
+=head1 AUTHOR
+
+Kyle M Hall <kyle@kylehall.info> 
+
+=cut
+
+=head1 LICENSE
+This file is part of Libki.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.   
+
+=cut
