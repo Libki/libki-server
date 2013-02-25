6c68779f0bacf351fd1e95d2b5a8b6c40b223a2e
Kyle M Hall <kyle@kylehall.info>
2013-02-25 11:16:41 -0500

    Fix DateTime not using timezone 'local', incorrect timestamp calculation

--- script/cronjobs/libki.pl
+++ script/cronjobs/libki.pl
@@ -24,7 +24,6 @@ my $connect_info = $config_hash->{'Model::DB'}->{'connect_info'};
 my $schema = Libki::Schema::DB->connect($connect_info)
   || die("Couldn't Connect to DB");
 
-
 ## Decrement time for logged in users.
 my $session_rs = $schema->resultset('Session');
 while ( my $session = $session_rs->next() ) {
@@ -34,13 +33,12 @@ while ( my $session = $session_rs->next() ) {
     }
 }
 
-
 ## Delete clients that haven't updated recently
 my $post_crash_timeout =
   $schema->resultset('Setting')->find('PostCrashTimeout')->value;
 
 my $timestamp = DateTime::Format::MySQL->format_datetime(
-    DateTime->now()->add_duration(
+    DateTime->now( time_zone => 'local' )->subtract_duration(
         DateTime::Duration->new( minutes => $post_crash_timeout )
     )
 );
@@ -48,15 +46,17 @@ my $timestamp = DateTime::Format::MySQL->format_datetime(
 $schema->resultset('Client')
   ->search( { last_registered => { '<', $timestamp } } )->delete();
 
-
 ## Clear out any expired reservations
 $schema->resultset('Reservation')->search(
     {
-        'expiration' =>
-          { '<', DateTime::Format::MySQL->format_datetime( DateTime->now( time_zone => 'local' ) ) }
+        'expiration' => {
+            '<',
+            DateTime::Format::MySQL->format_datetime(
+                DateTime->now( time_zone => 'local' )
+            )
+        }
     }
 )->delete();
-
 
 =head1 AUTHOR
 
