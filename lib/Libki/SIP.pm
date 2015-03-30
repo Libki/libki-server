e671c91450bfdc8b85a0a653b4d8a337085b814f
root <root@enu-test>
2015-03-30 15:46:47 +0100

    SIP2 modifications. This is the first step in making the application SIP2 compliant. - Added 'require_sip_auth' to libki_local.conf - Added 'enable_split_messages' to libki_local.conf - Changed typo in libki_local.conf from 'separator' to 'terminator' - Modified SIP.pm to facilitate options in libki_local.conf, allowing for various SIP2 options

--- lib/Libki/SIP.pm
+++ lib/Libki/SIP.pm
@@ -2,86 +2,143 @@ package Libki::SIP;
 
 use Socket qw(:crlf);
 use IO::Socket::INET;
+use POSIX qw(strftime);
+use Data::Dumper;
 
 sub authenticate_via_sip {
     my ( $c, $user, $username, $password ) = @_;
-
-    my $terminator = ( $c->config->{SIP}->{terminator} eq 'CR' ) ? $CR : $CRLF;
-    local $/ = $terminator;
-
-    my ( $sec, $min, $hour, $day, $month, $year ) = localtime(time);
-    $year += 1900;
-    my $transaction_date = "$year$month$day    $hour$min$sec";
-
-    my $host = $c->config->{SIP}->{host};
-    my $port = $c->config->{SIP}->{port};
-
-    my $login_user_id  = $c->config->{SIP}->{username};
-    my $login_password = $c->config->{SIP}->{password};
-    my $location_code  = $c->config->{SIP}->{location};
-
-    my $institution_id    = $location_code;
-    my $patron_identifier = $username;
-    my $terminal_password = $login_password;
-    my $patron_password   = $password;
-
-    my $summary = '          ';
-
+    my $terminator;
+    my $data;
+    my $transaction_date = timestamp();
+    my $host             = $c->config->{SIP}->{host};
+    my $port             = $c->config->{SIP}->{port};
+    my $patron_status_request;
+    if ( $c->config->{SIP}->{terminator} eq "NL" ) {
+        $terminator = chr(0x0a);
+    }
+    else { $terminator = chr(0x0d) }
     my $socket = IO::Socket::INET->new(
-            PeerAddr=>$host,
-            PeerPort=>$port
+        PeerAddr => $host,
+        PeerPort => $port,
+        Proto    => 'tcp',
+        Timeout  => '15',
+        Type     => SOCK_STREAM
     ) or die "ERROR in Socket Creation : $!\n";
+    if ( $c->config->{SIP}->{require_sip_auth} == 0 ) {
+        my $run_num = 0;
+        my $string  = "9900302.00";
+        my $final   = checksum( $string, $run_num );
+        my $send99  = $string . $final . $terminator;
+        $socket->send($send99);
+        my ( $response, $split );
+        $socket->recv( $response, 1024 );
+        chomp $response;
 
-    my $login_command =
-      "9300CN$login_user_id|CO$login_password|CP$location_code|";
+        if ( $c->config->{SIP}->{enable_split_messages} ) {
+            $socket->recv( $split, 1024 );
+            chomp $split;
+            $response .= $split;
+        }
+        if ( ( substr $response, 0, 3 ) eq '98Y' ) {
+            my ( $chk, $end ) = split /\|AY/, $response;
+            $chk .= "|AY";
+            my $run_num = substr( $end, 0, 1 );
+            $patron_status_request = talk63( $c->config->{SIP}->{location},
+                $username, $password, $run_num )
+              . $terminator;
+        }
+    }
+    if ( $c->config->{SIP}->{require_sip_auth} != 0 ) {
+        my $string = "9300";
+        $string .= "CN" . $c->config->{SIP}->{username} . "|";
+        $string .= "CO" . $c->config->{SIP}->{password} . "|";
+        $string .= "CP";
+        if ( $c->config->{SIP}->{location} ) {
+            $string .= $c->config->{SIP}->{location} . "|";
+        }
+        else {
+            $string .= "" . "|";
+        }
+        my $str_93 = checksum($string);
+        $str_93 = $string . $str_93 . $terminator;
+        $socket->send($str_93);
+        my $response;
+        $socket->recv( $response, 1024 );
+        chomp $response;
+        if ( $c->config->{SIP}->{enable_split_messages} ) {
+            $socket->recv( $split, 1024 );
+            chomp $split;
+            $response .= $split;
+        }
+        my $auth    = substr $response, 2, 1;
+        my $run_num = substr $response, 5, 1;
+        if ( $auth == "1" ) {
+            my $string = "9900302.00";
+            my $final  = checksum( $string, $run_num );
+            my $send99 = $string . $final . $terminator;
+            $socket->send($send99);
+            my ( $response, $split );
+            $socket->recv( $response, 1024 );
+            chomp $response;
+            if ( $c->config->{SIP}->{enable_split_messages} ) {
+                $socket->recv( $split, 1024 );
+                chomp $split;
+                $response .= $split;
+            }
+            if ( ( substr $response, 0, 3 ) eq '98Y' ) {
+                my ( $chk, $end ) = split /\|AY/, $response;
+                $chk .= "|AY";
+                my $run_num = substr( $end, 0, 1 );
+                $patron_status_request = talk63( $c->config->{SIP}->{location},
+                    $username, $password, $run_num )
+                  . $terminator;
+            }
+        }
+        else {
+            return { success => 0, error => 'SIP_AUTH_FAILURE', user => $user };
+        }
+    }
 
-    print $socket $login_command . $terminator;
-    my $data = <$socket>;
-    if ( $data =~ '^941' ) {
-        my $patron_status_request =
-            "63001" 
-          . $summary
-          . $transaction_date . "AO"
-          . $institution_id . "|AA"
-          . $patron_identifier . "|AC"
-          . $terminal_password . "|AD"
-          . $patron_password . "|";
-        print $socket $patron_status_request . $terminator;
+    $socket->send( $patron_status_request . $terminator );
+    $socket->recv( $data, 1024 );
+    if ( $c->config->{SIP}->{enable_split_messages} ) {
+        $socket->recv( $split, 1024 );
+        chomp $split;
+        $data .= $split;
+    }
+    if ( CORE::index( $data, 'BLY' ) != -1 ) {
+        if ( CORE::index( $data, 'CQY' ) != -1 ) {
 
-        $data = <$socket>;
-        if ( CORE::index( $data, 'BLY' ) != -1 ) {
-            if ( CORE::index( $data, 'CQY' ) != -1 ) {
+            if ($user) {    ## User authenticated and exists in Libki
+                $user->set_column( 'password', $password );
+                $user->update();
+            }
+            else {          ## User authenticated and does not exits in Libki
+                my $minutes =
+                  $c->model('DB::Setting')->find('DefaultTimeAllowance')->value;
 
-                if ($user) {    ## User authenticated and exists in Libki
-                    $user->set_column( 'password', $password );
-                    $user->update();
-                }
-                else {    ## User authenticated and does not exits in Libki
-                    my $minutes =
-                      $c->model('DB::Setting')->find('DefaultTimeAllowance')
-                      ->value;
+                $user = $c->model('DB::User')->create(
+                    {
+                        username          => $username,
+                        password          => $password,
+                        minutes_allotment => $minutes,
+                        status            => 'enabled',
+                    }
+                );
+            }
 
-                    $user = $c->model('DB::User')->create(
-                        {
-                            username          => $username,
-                            password          => $password,
-                            minutes_allotment => $minutes,
-                            status            => 'enabled',
-                        }
-                    );
-                }
+            my $sip_fields = sip_message_to_hashref($data);
 
-                my $sip_fields = sip_message_to_hashref($data);
+            if ( my $deny_on = $c->config->{SIP}->{deny_on} ) {
+                my @deny_on = ref($deny_on) eq "ARRAY" ? @$deny_on : $deny_on;
 
-                if ( my $deny_on = $c->config->{SIP}->{deny_on} ) {
-                    my @deny_on = ref($deny_on) eq "ARRAY" ? @$deny_on : $deny_on;
-
-                    foreach my $d (@deny_on) {
-                        if ( $sip_fields->{patron_status}->{$d} eq 'Y' ) {
-                            return { success => 0, error => uc($d), user => $user };
-                        }
+                foreach my $d (@deny_on) {
+                    if ( $sip_fields->{patron_status}->{$d} eq 'Y' ) {
+                        return { success => 0, error => uc($d), user => $user };
                     }
                 }
+
+                #}
 
                 if ( my $fee_limit = $c->config->{SIP}->{fee_limit} ) {
 
@@ -90,14 +147,23 @@ sub authenticate_via_sip {
                       if ( $fee_limit =~ /[A-Z][A-Z]/ );
 
                     if ( $sip_fields->{BV} > $fee_limit ) {
-                        return { success => 0, error => 'FEE_LIMIT', details => { fee_limit => $fee_limit }, user => $user };
+                        return {
+                            success => 0,
+                            error   => 'FEE_LIMIT',
+                            details => { fee_limit => $fee_limit },
+                            user    => $user
+                        };
                     }
                 }
 
                 return { success => 1, user => $user };
             }
             else {
-                return { success => 0, error => 'INVALID_PASSWORD', user => $user };
+                return {
+                    success => 0,
+                    error   => 'INVALID_PASSWORD',
+                    user    => $user
+                };
             }
         }
         else {
@@ -157,6 +223,40 @@ sub sip_message_to_hashref {
     $fields{patron_status} = $patron_status;
 
     return \%fields;
+}
+
+sub timestamp {
+    my $timestamp = strftime '%Y%m%d    %H%M%S', localtime;
+    return $timestamp;
+}
+
+sub checksum {
+    my $str     = shift;
+    my $run_num = shift;
+    if ( $run_num == 10 ) {
+        $run_num = 0;
+    }
+    unless ($run_num) {
+        $run_num = '0';
+    }
+    $run_num++;
+    my $trail = "AY$run_num" . 'AZ';
+    $str .= $trail;
+    my $checksum = -unpack( '%16U*', $str ) & 0xFFFF;
+    $trail .= sprintf '%04.4X', $checksum;
+    return $trail;
+}
+
+sub talk63 {
+    my ( $location, $userid, $pin, $run_num ) = @_;
+    my $summary = "          ";
+    my $str     = "63" . "001" . timestamp() . $summary;
+    $str .= "AO" . $location . "|" . "AA$userid" . "|" . "AC" . "|";
+    if ($pin) {
+        $str .= "AD" . $pin . "|";
+    }
+    my $resp = checksum( $str, $run_num );
+    return ( $str . $resp );
 }
 
 1;
