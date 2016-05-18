8815fd915df8d217fe2021974ac49de8263ac7da
Erik Öhrn <8508226217@sunneadm.local>
2016-05-18 15:40:14 +0200

    Changes in log file handling

--- scheduler.bat
+++ scheduler.bat
@@ -3,3 +3,9 @@ REM Schedules cronjobs
 schtasks /create /sc minute /mo 1 /tn "Libki timer" /tr "C:\libki-server\win_scheduler\libki.bat" /ru System
 schtasks /create /sc daily /st 00:00:00 /tn "Libki nightly" /tr "C:\libki-server\win_scheduler\libki_nightly.bat" /ru System
 schtasks /create /sc onstart /tn "Libki server on boot" /tr "C:\libki-server\win_scheduler\libki_server.bat" /ru System
+
+REM Adds log file
+
+mkdir C:\home\libki\libki-server
+
+copy /y nul C:\home\libki\libki-server\libki.log
