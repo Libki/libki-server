9a57ce9b805ee8fafd2b898a0ee20614a214739b
loidor <erik.ohrn@gmail.com>
2016-10-12 13:15:00 +0200

    Update History.pm

--- lib/Libki/Controller/Administration/History.pm
+++ lib/Libki/Controller/Administration/History.pm
@@ -2,6 +2,8 @@ package Libki::Controller::Administration::History;
 use Moose;
 use namespace::autoclean;
 
+use Encode qw(decode encode);
+
 BEGIN { extends 'Catalyst::Controller'; }
 
 =head1 NAME
@@ -65,11 +67,13 @@ sub statistics : Local : Args(0) {
             ],
         }
     );
+    
+    my $enc = 'UTF-8';
 
     my $results;
     my $columns;
     foreach my $b (@by_location) {
-        my %columns = $b->get_columns;
+        my %columns = decode($enc,$b->get_columns);
         $columns{'location'} = "XXX__UNDEFINED__"
           unless ( defined( $columns{'location'} ) );
         $results->{ $columns{'year'} . '-'
