ce0adaa4ccfc02c475ab11d43e5421bba06c4f15
Kyle M Hall <kyle@bywatetsolutions.com>
2018-04-16 14:36:23 -0400

    Clean up cpanfile, add Module::Install to basic_setup.sh before using cpanm --installdeps

--- script/setup/basic_setup.sh
+++ script/setup/basic_setup.sh
@@ -6,4 +6,5 @@ touch /home/libki/libki_server.log
 curl -L http://cpanmin.us | perl - -l /home/libki/perl5 App::cpanminus local::lib
 eval `perl -I ~/perl5/lib/perl5 -Mlocal::lib=/home/libki/perl5`
 echo 'eval $(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)' >> /home/libki/.bashrc
+cpanm -n Module::Install
 cpanm -n --installdeps .
