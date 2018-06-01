881700de90cc9d913d85d264cfd295c13a83636c
Kyle M Hall <kyle@bywatetsolutions.com>
2018-06-01 12:05:58 -0400

    Update script update_schema.sh

--- script/development/update_schema.sh
+++ script/development/update_schema.sh
@@ -3,8 +3,8 @@
 if [ $1 ]
 then
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-    $DIR/../libki_create.pl model DB DBIC::Schema Libki::Schema::DB create=static components=TimeStamp,EncodedColumn,Numeric dbi:mysql:$1 $2 $3
+    $DIR/../libki_create.pl model DB DBIC::Schema Libki::Schema::DB create=static components=TimeStamp,EncodedColumn,Numeric dbi:mysql:$1:$2:$3 $4 $5
 else
-    echo "update_schema.sh database username password"
+    echo "update_schema.sh database host port username password"
 fi
 
