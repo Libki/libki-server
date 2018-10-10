85e5bb8a426f08453f67e9a1c1c5c72afa740e7d
Kyle M Hall <kyle@bywatersolutions.com>
2018-10-10 14:28:21 -0400

    Add cli sip tester

---
+++ script/administration/test_sip.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+use Modern::Perl;
+
+use Getopt::Long::Descriptive;
+
+use Libki;
+use Libki::SIP qw( authenticate_via_sip );
+
+use Data::Dumper;
+
+my ( $opt, $usage ) = describe_options(
+    '%c %o',
+    [ 'instance|i=s', "the instance for the user to exist on", { default => q{} } ],
+    [ 'username|u=s', "the username for this user, required", { required => 1 } ],
+    [ 'password|p=s', "the password for this user" ],
+    [],
+    [ 'verbose|v', "print extra stuff" ],
+);
+
+print( $usage->text ), exit unless ( $opt->username );
+
+$ENV{LIBKI_INSTANCE} = $opt->instance || q{};
+
+my $c = Libki->new();
+
+my $user = $user_rs->search( { instance => $opt->instance, username => $opt->username } )->next();
+
+my $r = Libki::SIP::authenticate_via_sip( $c, $user, $opt->username, $opt->password );
+
+print "Libki::SIP::authenticate_via_sip: " . Data::Dumper::Dumper( $r );
+
+=head1 AUTHOR
+
+Kyle M Hall <kyle@kylehall.info> 
+
+=cut
+
+=head1 LICENSE
+This file is part of Libki.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.   
+
+=cut
