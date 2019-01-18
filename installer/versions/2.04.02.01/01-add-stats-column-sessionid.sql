573dc3ac6fb0fd3fc8a076afc689af3068cb4705
Kyle M Hall <kyle@bywatersolutions.com>
2019-01-18 03:53:15 -0800

    Server Issue #73: Add missing database column statistics.sessionid
    https://github.com/Libki/libki-server/issues/73

---
+++ installer/versions/2.04.02.01/01-add-stats-column-sessionid.sql
@@ -0,0 +1,2 @@
+-- Foreign keys to login_sessions, need to survive deletion of login_sessions rows, so no constraint implemented
+ALTER TABLE `statistics` ADD COLUMN `session_id` CHAR(72) NULL DEFAULT NULL;
