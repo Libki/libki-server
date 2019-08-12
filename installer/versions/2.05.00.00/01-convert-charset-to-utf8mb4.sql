daba85bba5ba603814f79c9980ce80df676193e5
loidor <erik.ohrn@gmail.com>
2019-08-12 22:35:25 +0200

    Added CONVERT TO since it was needed to convert columns as well.

--- installer/versions/2.05.00.00/01-convert-charset-to-utf8mb4.sql
+++ installer/versions/2.05.00.00/01-convert-charset-to-utf8mb4.sql
@@ -2,20 +2,19 @@
 
 ALTER DATABASE libki CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE client_age_limits CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE clients CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE closing_hours CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE jobs CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE locations CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE login_sessions CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE messages CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE print_files CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE print_jobs CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE reservations CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE roles CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE sessions CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE settings CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE statistics CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE user_roles CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE users CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+ALTER TABLE client_age_limits CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE clients CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE closing_hours CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE jobs CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE locations CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE login_sessions CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE messages CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE print_files CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE print_jobs CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE reservations CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE roles CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE sessions CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE settings CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE statistics CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE user_roles CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE users CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
