9786a0572182f09228e5e8a16877847151ba5e8d
Bin Wen <bin.wen@inlibro.com>
2020-04-09 08:45:40 -0400

    Add user login access, simplify login for reservation and cancellation

---
+++ lib/Libki/Controller/Public/Login.pm
@@ -0,0 +1,108 @@
+package Libki::Controller::Public::Login;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+LibkiServer::Controller::Public::Login - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+=head2 index
+
+Login logic
+
+=cut
+
+sub index : Path : Args(0) {
+    my ( $self, $c ) = @_;
+
+    # Get the username and password from form
+    my $username  = $c->request->params->{username};
+    my $password  = $c->request->params->{password};
+    my $submitted = $c->request->params->{submitted} || 0;
+
+    my $instance = $c->instance;
+    my $auth = 1;
+    my $config = $c->instance_config;
+    # If the username and password values were found in form
+    if ( $username && $password ) {
+        my $user = $c->model('DB::User')->single( { instance => $instance, username => $username } );
+        if ( $config->{SIP}->{enable} ) {
+            if ( !$user || ( $user && $user->is_guest() eq 'No' )) {
+                my $ret = Libki::SIP::authenticate_via_sip( $c, $user, $username, $password );
+                $auth = $ret->{success};
+            }
+        }
+        elsif ( $config->{LDAP}->{enable} ) {
+            if ( !$user || ( $user && $user->is_guest() eq 'No' )) {
+                my $ret = Libki::LDAP::authenticate_via_ldap( $c, $user, $username, $password );
+                $auth = $ret->{success};
+            }
+        }
+
+        $auth = 0 if ( $user && $user->is_guest() eq 'Yes' );
+
+        if ( $auth ) {
+            $auth = $c->authenticate( { username => $username, password => $password, instance => $instance } );
+        }
+
+        if ( $auth ) {
+            $c->response->redirect(
+                $c->uri_for(
+                    $c->controller('Public')->action_for('index')
+                )
+            );
+        }
+        else {
+            $c->stash( error_message => "Wrong username or password." );
+        }
+    }
+    else {
+        if ($submitted) {
+
+            # Set an error message
+            $c->stash( error_message => "Empty username or password." );
+        }
+    }
+
+    # If either of above don't work out, send to the login page
+    $c->stash( template => 'public/login.tt' );
+}
+
+=head1 AUTHOR
+
+Kyle M Hall <kyle@kylehall.info>
+
+=cut
+
+=head1 LICENSE
+
+This file is part of Libki.
+
+Libki is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as 
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+                
+Libki is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+                                                
+You should have received a copy of the GNU General Public License
+along with Libki.  If not, see <http://www.gnu.org/licenses/>.
+                                                
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ lib/Libki/Controller/Public/Logout.pm
@@ -0,0 +1,62 @@
+package Libki::Controller::Public::Logout;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Libki::Controller::Public::Logout - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    # Clear the user's state
+    $c->logout;
+
+    # Send the user to the starting point
+    $c->response->redirect( $c->uri_for('/public') );
+}
+
+
+=head1 AUTHOR
+
+Kyle M Hall <kyle@kylehall.info>
+
+=cut
+
+=head1 LICENSE
+
+This file is part of Libki.
+
+Libki is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as 
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+                
+Libki is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+                                                
+You should have received a copy of the GNU General Public License
+along with Libki.  If not, see <http://www.gnu.org/licenses/>.
+                                                
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
