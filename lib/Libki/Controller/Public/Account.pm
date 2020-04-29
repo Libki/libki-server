9fc8f0fc7ecb4f17ff025620745336c1b88ffd54
Bin Wen <bin.wen@inlibro.com>
2020-04-29 10:29:28 -0400

    new feature for user to manage their reservation

---
+++ lib/Libki/Controller/Public/Account.pm
@@ -0,0 +1,86 @@
+package Libki::Controller::Public::Account;
+use Moose;
+use namespace::autoclean;
+use Date::Parse;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Libki::Controller::Public::Account - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+    if ($c->user_exists()) {
+        my $reservation = $c->model('DB::Reservation')->find({ user_id => $c->user->id()}) || 0;
+        my ($begin, $end, $duration) = (undef, undef, 0);
+        if($reservation){
+            $begin = $reservation->begin_time;
+            $end = $reservation->end_time;
+            $begin =~ s/T/ /;
+            $end   =~ s/T/ /;
+            $duration = ( str2time($end) - str2time($begin) ) / 60;
+        }
+        $c->stash( 'reservation' => $reservation, 'template' => 'public/account.tt', 'begin' => $begin, 'end' => $end, 'duration' => $duration );
+    }
+    else {
+        $c->response->redirect( $c->uri_for('/public') );
+    }
+}
+
+=head2 cancel
+
+    Cancel user's reservation
+
+=cut
+
+sub cancel :Local :Args(0) {
+    my ( $self, $c ) = @_;
+    if ($c->user_exists()){
+        my $reservation = $c->model('DB::Reservation')->find({ user_id => $c->user->id()});
+        if( $reservation) {
+            $c->response->redirect( $c->uri_for('/public/account') ) if( $reservation->delete() );
+        }
+    }     
+}
+
+=head1 AUTHOR
+
+Kyle M Hall <kyle@kylehall.info>
+
+=cut
+
+=head1 LICENSE
+
+This file is part of Libki.
+
+Libki is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as 
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+Libki is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Libki.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
