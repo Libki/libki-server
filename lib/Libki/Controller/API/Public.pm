6d5c47addb53f1fc488bf1873f8a96a9f63d92cd
Kyle M Hall <kyle@bywatersolutions.com>
2021-06-09 08:11:41 -0400

    Add 'funds' api

--- lib/Libki/Controller/API/Public.pm
+++ lib/Libki/Controller/API/Public.pm
@@ -25,14 +25,18 @@ Catalyst Controller.
 
 =head2 authenticate
 
-/api/public/authenticate/<api_key>?username=admin&password=mypass
+/api/public/authenticate?api_key=API_KEY&username=admin&password=mypass
 
 Returns JSON with keys for success and error.
 
 =cut
 
-sub authenticate : Local : Args(1) {
-    my ( $self, $c, $api_key ) = @_;
+sub authenticate_user : Local : Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $api_key  = $c->request->params->{'api_key'};
+    my $username = $c->request->params->{'username'};
+    my $password = $c->request->params->{'password'};
 
     my $api_key_validated = Libki::Auth::validate_api_key(
         {
@@ -41,9 +45,6 @@ sub authenticate : Local : Args(1) {
             type    => '*',
         }
     );
-
-    my $username = $c->request->params->{'username'};
-    my $password = $c->request->params->{'password'};
 
     $c->log()->debug("API::Public::authenticate( username => $username, username => $username");
 
@@ -65,16 +66,84 @@ sub authenticate : Local : Args(1) {
         );
     }
     elsif ( !$api_key_validated ) {
+        $c->response->status(401);
         $c->stash(
             success => JSON::false,
-            error   => "Invalid API key.",
+            error   => "INVALID_API_KEY",
         );
     }
     else {
+        $c->response->status(404);
         $c->stash(
             success => JSON::false,
-            error   => "Parameters username and password are required.",
+            error   => "MISSING_PARAMETERS",
         );
+    }
+
+    delete $c->stash->{Settings};
+    $c->forward( $c->view('JSON') );
+}
+
+=head2 funds
+
+GET /api/public/funds?api_key=API_KEY&username=someuser
+
+POST /api/public/funds/api_key=API_KEY&username=someuser&amount=1.25
+
+Returns JSON with keys for success and error.
+
+=cut
+
+sub user_funds : Local : Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $api_key  = $c->request->params->{'api_key'};
+    my $username = $c->request->params->{'username'};
+
+    my $api_key_validated = Libki::Auth::validate_api_key(
+        {
+            context => $c,
+            key     => $api_key,
+            type    => '*',
+        }
+    );
+
+    my $instance = $c->instance;
+    my $user = $username ? $c->model('DB::User')->find({ instance => $instance, username => $username }) : undef;
+
+    if ( !$api_key_validated ) {
+        $c->response->status(401);
+        $c->stash(
+            success => JSON::false,
+            error   => "INVALID_API_KEY",
+        );
+    } elsif ( !$user ) {
+        $c->response->status(404);
+        $c->stash(
+            success => JSON::false,
+            error   => "INVALID_USER",
+        );
+    } else {
+        if ( $c->request->method eq 'GET' ) {
+            $c->stash(
+                success => JSON::true,
+                balance => $user->funds,
+            );
+        } elsif ( $c->request->method eq 'POST' ) {
+            my $funds = $c->request->params->{'funds'};
+            $user->funds( $user->funds + $funds );
+            $user->update();
+            $c->stash(
+                success => JSON::true,
+                balance => $user->funds,
+            );
+        } else {
+            $c->response->status(404);
+            $c->stash(
+                success => JSON::false,
+                error   => "Invalid HTTP verb.",
+            );
+        }
     }
 
     delete $c->stash->{Settings};
