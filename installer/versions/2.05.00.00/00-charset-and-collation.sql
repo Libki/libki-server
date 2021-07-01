824969193c7546ae078cf61162da364d9a1e3ee6
Kyle M Hall <kyle@bywatersolutions.com>
2021-07-01 07:34:35 -0400

    Remove hardcoded database name 'libki' from ALTER TABLE statement

--- installer/versions/2.05.00.00/00-charset-and-collation.sql
+++ installer/versions/2.05.00.00/00-charset-and-collation.sql
@@ -1,5 +1,5 @@
 -- Convert everything to utf8mb4
-ALTER DATABASE libki CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE client_age_limits CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE clients CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
