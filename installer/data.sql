dd3498856da468629ad8fcc3d0ed8373637a55ee
Kyle M Hall <kyle@bywatersolutions.com>
2022-03-21 08:35:50 -0400

    Export default data via update_schema

---
+++ installer/data.sql
@@ -0,0 +1,189 @@
+-- MariaDB dump 10.19  Distrib 10.5.12-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: libki-mariadb    Database: libki
+-- ------------------------------------------------------
+-- Server version	10.3.5-MariaDB-10.3.5+maria~jessie
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `allotments`
+--
+
+LOCK TABLES `allotments` WRITE;
+/*!40000 ALTER TABLE `allotments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `allotments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `client_age_limits`
+--
+
+LOCK TABLES `client_age_limits` WRITE;
+/*!40000 ALTER TABLE `client_age_limits` DISABLE KEYS */;
+/*!40000 ALTER TABLE `client_age_limits` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `closing_hours`
+--
+
+LOCK TABLES `closing_hours` WRITE;
+/*!40000 ALTER TABLE `closing_hours` DISABLE KEYS */;
+/*!40000 ALTER TABLE `closing_hours` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `jobs`
+--
+
+LOCK TABLES `jobs` WRITE;
+/*!40000 ALTER TABLE `jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jobs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `locations`
+--
+
+LOCK TABLES `locations` WRITE;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `login_sessions`
+--
+
+LOCK TABLES `login_sessions` WRITE;
+/*!40000 ALTER TABLE `login_sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `login_sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `logs`
+--
+
+LOCK TABLES `logs` WRITE;
+/*!40000 ALTER TABLE `logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `messages`
+--
+
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `print_files`
+--
+
+LOCK TABLES `print_files` WRITE;
+/*!40000 ALTER TABLE `print_files` DISABLE KEYS */;
+/*!40000 ALTER TABLE `print_files` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `print_jobs`
+--
+
+LOCK TABLES `print_jobs` WRITE;
+/*!40000 ALTER TABLE `print_jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `print_jobs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `reservations`
+--
+
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'user'),(2,'admin'),(3,'superadmin');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `sessions`
+--
+
+LOCK TABLES `sessions` WRITE;
+/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `settings`
+--
+
+LOCK TABLES `settings` WRITE;
+/*!40000 ALTER TABLE `settings` DISABLE KEYS */;
+INSERT INTO `settings` VALUES ('','AutomaticTimeExtensionAt',''),('','AutomaticTimeExtensionLength',''),('','AutomaticTimeExtensionRenewal','0'),('','AutomaticTimeExtensionUnless','this_reserved'),('','AutomaticTimeExtensionUseAllotment','no'),('','BatchGuestPassCustomCSS','body { /* default body style emulates a pre tag */\n    font-family: monospace;\n    white-space: pre;\n    display: block;\n    unicode-bidi: embed;\n}\n.guest-pass { /* each username and password is in a guest-pass span */\n    /* page-break-before: always; */ /* This will cause each pass to have a page break, good for use with receipt printers */\n}\n.guest-pass-username {} /* span containing the username label and the username itself */\n.guest-pass-username-label {} /* span containing the username label */\n.guest-pass-username-content {} /* span containing the username itself */\n.guest-pass-password {} /* span containing the password label and the password itself */\n.guest-pass-password-label {} /* span containing the password label */\n.guest-pass-password-content {} /* span containing the password itself */\n'),('','BatchGuestPassPasswordLabel','Computer Guest Pass     Password=  '),('','BatchGuestPassTemplate','<html>\n  <head>\n    <style type=\"text/css\">[% batch_guest_pass_custom_css %]</style>\n  </head>\n  <body>\n    [% FOREACH g IN guests %]\n      <p class=\"guest-pass\">\n        <p class=\"guest-pass-username\">\n          <span class=\"guest-pass-username-label\">[% batch_guest_pass_username_label %]</span><span class=\"guest-pass-username-content\">[% g.username %]</span>\n        </p>\n        <p class=\"guest-pass-password\">\n          <span class=\"guest-pass-password-label\">[% batch_guest_pass_password_label %]</span><span class=\"guest-pass-password-content\">[%g. password %]</span>\n        </p>\n      </p>\n      <br/>\n    [% END %]\n  </body>\n</html>'),('','BatchGuestPassUsernameLabel','Your Library            Username=  '),('','ClientBehavior','FCFS+RES'),('','CurrentGuestNumber','1'),('','CustomJsAdministration',''),('','CustomJsPublic',''),('','DataRetentionDays',''),('','DefaultGuestSessionTimeAllowance','45'),('','DefaultGuestTimeAllowance','45'),('','DefaultSessionTimeAllowance','45'),('','DefaultTimeAllowance','45'),('','DisplayReservationStatusWithin','60'),('','GuestBatchCount','40'),('','MinimumReservationMinutes','5'),('','PostCrashTimeout','5'),('','PrintJobRetentionDays','0'),('','ReservationShowUsername','RSD'),('','ReservationTimeout','15'),('','ShowFirstLastNames','1'),('','ThirdPartyURL',''),('','UserCategories',''),('','Version','4.1.0');
+/*!40000 ALTER TABLE `settings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `statistics`
+--
+
+LOCK TABLES `statistics` WRITE;
+/*!40000 ALTER TABLE `statistics` DISABLE KEYS */;
+/*!40000 ALTER TABLE `statistics` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-21  8:32:59
--- script/development/update_schema.sh
+++ script/development/update_schema.sh
@@ -4,12 +4,54 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ $1 ]
 then
-    $DIR/../libki_create.pl model DB DBIC::Schema Libki::Schema::DB create=static components=TimeStamp,EncodedColumn,Numeric dbi:mysql:$1:$2:$3 $4 $5 cursor_class=DBIx::Class::Cursor::Cached overwrite_modifications=1
-    mysqldump -d -u $4 -p$5 -h $2 -P $3 $1 | sed 's/ AUTO_INCREMENT=[0-9]*//g' > $DIR/../../installer/schema.sql
-elif [[ $LIBKI_DB_DATABASE && $LIBKI_DB_HOST && $LIBKI_DB_PORT && $LIBKI_DB_USER && $LIBKI_DB_PASSWORD ]]
+    LIBKI_DB_DATABASE=$1
+    LIBKI_DB_HOST=$2
+    LIBKI_DB_PORT=$3
+    LIBKI_DB_USER=$4
+    LIBKI_DB_PASSWORD=$5
+fi
+
+if [[ $LIBKI_DB_DATABASE && $LIBKI_DB_HOST && $LIBKI_DB_PORT && $LIBKI_DB_USER && $LIBKI_DB_PASSWORD ]]
 then
+    # Update the Libki schema files
     $DIR/../libki_create.pl model DB DBIC::Schema Libki::Schema::DB create=static components=TimeStamp,EncodedColumn,Numeric dbi:mysql:$LIBKI_DB_DATABASE:$LIBKI_DB_HOST:$LIBKI_DB_PORT $LIBKI_DB_USER $LIBKI_DB_PASSWORD cursor_class=DBIx::Class::Cursor::Cached overwrite_modifications=1
-    mysqldump -d -u $LIBKI_DB_USER -p$LIBKI_DB_PASSWORD -h $LIBKI_DB_HOST -P $LIBKI_DB_PORT $LIBKI_DB_DATABASE | sed 's/ AUTO_INCREMENT=[0-9]*//g' > $DIR/../../installer/schema.sql
+
+    # Save a copy of the current database schema
+    mysqldump --no-data -u $LIBKI_DB_USER -p$LIBKI_DB_PASSWORD -h $LIBKI_DB_HOST -P $LIBKI_DB_PORT $LIBKI_DB_DATABASE | sed 's/ AUTO_INCREMENT=[0-9]*//g' > /tmp/libki_current_schema.sql
+
+    # Save a copy of the current database data
+    mysqldump --no-create-info -u $LIBKI_DB_USER -p$LIBKI_DB_PASSWORD -h $LIBKI_DB_HOST -P $LIBKI_DB_PORT $LIBKI_DB_DATABASE | sed 's/ AUTO_INCREMENT=[0-9]*//g' > /tmp/libki_current_data.sql
+
+    # Drop and recreate the empty database
+    mysql -u $LIBKI_DB_USER -p$LIBKI_DB_PASSWORD -h $LIBKI_DB_HOST -P $LIBKI_DB_PORT $LIBKI_DB_DATABASE -e "DROP DATABASE $LIBKI_DB_DATABASE; CREATE DATABASE $LIBKI_DB_DATABASE"
+
+    # Run the installer to generate a fresh db schema
+    $DIR/../../installer/update_db.pl
+
+    # Save a copy of the current database schema
+    mysqldump --no-data -u $LIBKI_DB_USER -p$LIBKI_DB_PASSWORD -h $LIBKI_DB_HOST -P $LIBKI_DB_PORT $LIBKI_DB_DATABASE | sed 's/ AUTO_INCREMENT=[0-9]*//g' > $DIR/../../installer/schema.sql
+
+    # Compare the two new schema files
+    DIFF=$(diff $DIR/../../installer/schema.sql /tmp/libki_current_schema.sql) 
+    if [ "$DIFF" != "" ]
+        # Save a copy of the updated default database data
+        mysqldump --no-create-info -u $LIBKI_DB_USER -p$LIBKI_DB_PASSWORD -h $LIBKI_DB_HOST -P $LIBKI_DB_PORT $LIBKI_DB_DATABASE | sed 's/ AUTO_INCREMENT=[0-9]*//g' > $DIR/../../installer/data.sql
+    then
+        echo "WARNING: The schema from a fresh install does not match the current schema. Did you forget to add a database update?"
+    fi
+
+    # Save a copy of the updated default database data
+    mysqldump --no-create-info -u $LIBKI_DB_USER -p$LIBKI_DB_PASSWORD -h $LIBKI_DB_HOST -P $LIBKI_DB_PORT $LIBKI_DB_DATABASE | sed 's/ AUTO_INCREMENT=[0-9]*//g' > $DIR/../../installer/data.sql
+
+    # Drop and recreate the empty database again
+    mysql -u $LIBKI_DB_USER -p$LIBKI_DB_PASSWORD -h $LIBKI_DB_HOST -P $LIBKI_DB_PORT $LIBKI_DB_DATABASE -e "DROP DATABASE $LIBKI_DB_DATABASE; CREATE DATABASE $LIBKI_DB_DATABASE"
+
+    # Restore the original database data and schame
+    mysql -u $LIBKI_DB_USER -p$LIBKI_DB_PASSWORD -h $LIBKI_DB_HOST -P $LIBKI_DB_PORT $LIBKI_DB_DATABASE < /tmp/libki_current_schema.sql
+    mysql -u $LIBKI_DB_USER -p$LIBKI_DB_PASSWORD -h $LIBKI_DB_HOST -P $LIBKI_DB_PORT $LIBKI_DB_DATABASE < /tmp/libki_current_data.sql
+
+    rm /tmp/libki_current_data.sql
+    rm /tmp/libki_current_schema.sql
 else
     echo "update_schema.sh database host port username password"
 fi
