6843fdb1eecab002fd225ebaa36f71052da9c050
Ian Walls <ian@bywatersolutions.com>
2026-01-16 14:27:45 -0500

    Discussion #419:  RESTful API v2
    Creates a new v2 route for the REST API; currently only used for
    Transactions, which are required plumbing for Stripe payments (#359).
    
    New API endpoints will be build here, and eventually older endpoints
    used by Server's web interface will move over.  Client, PrintManager and
    PrintStation APIs will remain as long as possible, for backwards compatibility

---
+++ lib/Libki/Controller/API/V2/Transactions/Stripe.pm
@@ -0,0 +1,50 @@
+package Libki::Controller::API::V2::Transactions::Stripe;
+
+use Moose;
+use namespace::autoclean;
+
+use Libki::Payments::Stripe;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+__PACKAGE__->config(
+    namespace => 'api/v2/transactions/stripe'
+);
+
+sub checkout : POST Path('checkout') Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $amount = $c->request->params->{amount};
+
+    return unless $self->_require_login($c);
+    return unless $self->_validate_positive_amount($c, $amount);
+
+    my $provider = Libki::Payments::Stripe->new;
+
+    my $result = $provider->create_checkout(
+        c        => $c,
+        user     => $c->user,
+        amount   => $amount,
+        instance => $c->instance,
+    );
+
+    $c->stash->{json} = {
+        checkout_url => $result->{checkout_url},
+    };
+
+    $c->stash->delete('Settings');
+    $c->forward('View::JSON');
+}
+
+sub webhook : POST Path('webhook') Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $provider = Libki::Payments::Stripe->new;
+    $provider->handle_webhook( c => $c );
+
+    $c->response->status(200);
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
+
